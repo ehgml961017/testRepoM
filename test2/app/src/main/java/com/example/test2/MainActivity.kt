c7bd7d9ea9bbe17fcf8ad6e7de9a8e3c12d6d235
dohee <ehgml961017@gmail.com>
2020-07-23 18:27:01 +0900

    card view for team4

--- test2/app/src/main/java/com/example/test2/MainActivity.kt
+++ test2/app/src/main/java/com/example/test2/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.test2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener(OnCompleteListener { task ->
+            if(!task.isSuccessful){
+                Log.i("testLog", "getInstanceId failed")
+                return@OnCompleteListener
+            }
+            Log.i("testLog", "getInstanceId")
+            val token = task.result?.token
+            Log.i("testLog", "token : $token")
+        })
+
+
     }
 }
